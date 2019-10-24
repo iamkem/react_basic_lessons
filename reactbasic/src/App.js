b1e6e6034246215eccabe24fbb05d008d6e342fe
mjnkool <hoangdinhthangloi@gmail.com>
2019-10-24 08:24:42 +0700

    Components & Props

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import sinhvien from './components/scripts/sinhvien';
+import SanPham from './components/scripts/SanPham'
 
 function App() {
     return ( <div className = "App">
-        <header className = "App-header">   
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"></link>     
-        <h1>Hello. Welcome to my chanel! </h1>
-        <div class="box">
-            <div class="container-1">
-                <span class="icon"><i class="fa fa-search"></i></span>
-                <input type="search" id="search" placeholder="Search..." />
-            </div>
-        </div>
-        <p>I'm Kem</p>
+        <header className = "App-header">  
+        <SanPham tensanPham = "MacBookPro 2014" masanPham = "1" giasanPham = "60.000.000">áhdjksa</SanPham>
+        <SanPham tensanPham = "MacBookPro 2015" masanPham = "2" giasanPham = "70.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2016" masanPham = "3" giasanPham = "80.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2017" masanPham = "4" giasanPham = "90.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2018" masanPham = "5" giasanPham = "100.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2019" masanPham = "6" giasanPham = "110.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2020" masanPham = "7" giasanPham = "120.000.000"></SanPham> 
+        <SanPham tensanPham = "MacBookPro 2021" masanPham = "8" giasanPham = "130.000.000"></SanPham>       
         </header>
-        <footer className = "App-footer">facebook.com/mjnkool</footer>
         </div>
     );
 }
-
 export default App;
